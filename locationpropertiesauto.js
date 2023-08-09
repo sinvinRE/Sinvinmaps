22a3a00c207c82bdc79fee6152e652963a544136
jagadishkpro <91598654+jagadishkpro@users.noreply.github.com>
2023-08-09 21:02:41 +0530

    Add files via upload

---
+++ locationpropertiesauto.js
@@ -0,0 +1,297 @@
+var storeJson = [
+    {
+        "Brand": "Lora Piana",
+        "Neighborhood": "World Trade Center",
+        "Address": "784 Madison",
+        "Full Address": "748 Madison Ave, New York, NY 10065",
+        "Latitude": 40.767286302901226,
+        "Longitude": -73.96898451166297
+    },
+    {
+        "Brand": "Lora Piana",
+        "Neighborhood": "Soho",
+        "Address": "438 West Broadway",
+        "Full Address": "3 9th Ave, New York, NY 10014",
+        "Latitude": 40.73959278859194, 
+        "Longitude": -74.00667584963361
+    },
+    {
+        "Brand": "Brunello Cucinelli",
+        "Neighborhood": "Soho",
+        "Address": "136 Greene St, New York, NY 10012",
+        "Full Address": "136 Greene St, New York, NY 10012",
+        "Latitude": 40.725493117667654,
+        "Longitude": -73.99840458766556
+    },
+    {
+        "Brand": "Brunello Cucinelli",
+        "Neighborhood": "Meatpacking",
+        "Address": "65D Wall Street",
+        "Full Address": "50 Gansevoort St, New York, NY 10014",
+        "Latitude": 40.73927263199088, 
+        "Longitude": -74.0068547576626
+    },
+    {
+        "Brand": "Brunello Cucinelli",
+        "Neighborhood": "Madison",
+        "Address": "14 West 29th Street",
+        "Full Address": "14 W 29th St, New York, NY 10001",
+        "Latitude": 40.765589454843564,
+        "Longitude": -73.97012463722066
+    },
+    {
+        "Brand": "Zegna",
+        "Neighborhood": "Midtown East",
+        "Address": "611 5th Ave",
+        "Full Address": "265 Lafayette St, New York, NY 10012",
+        "Latitude": 40.72612047791016,
+        "Longitude": -73.9962071040904
+    },
+    {
+        "Brand": "Zegna",
+        "Neighborhood": "Upper East Side",
+        "Address": "1000 Third Ave 59th Street",
+        "Full Address": "200 Vesey St Suite 198, New York, NY 10281",
+        "Latitude": 40.71673697871075,
+        "Longitude": -74.01445270186552
+    },
+    {
+        "Brand": "Zegna",
+        "Neighborhood": "5th Ave",
+        "Address": "611 5th Ave, New York, NY 10022",
+        "Full Address": "4 W 57th St, New York, NY 10019",
+        "Latitude": 40.76337996944491,
+        "Longitude": -73.97463840773136
+    },
+	{
+        "Brand": "Diptyque",
+        "Neighborhood": "Nolita",
+        "Address": "31 Prince Street",
+        "Full Address": "31 Prince St, New York, NY 10012",
+        "Latitude": 40.7230796,
+        "Longitude": -73.9947169
+    },
+    {
+        "Brand": "Diptyque",
+        "Neighborhood": "West Village",
+        "Address": "377 Bleecker",
+        "Full Address": "31 Prince St, New York, NY 10012",
+        "Latitude": 40.7230796,
+        "Longitude": -73.9947169
+    },
+    {
+        "Brand": "Diptyque",
+        "Neighborhood": "Grand Central",
+        "Address": "1 Grand Central Terminal",
+        "Full Address": "89 E 42nd St, New York, NY 10017",
+        "Latitude": 40.7527277,
+        "Longitude": -73.9772353
+    },
+    {
+        "Brand": "Diptyque",
+        "Neighborhood": "Williamsburg",
+        "Address": "229 Bedford Ave",
+        "Full Address": "229 Bedford Ave, Brooklyn, NY 11211",
+        "Latitude": 40.7162174,
+        "Longitude": -73.9593313
+    },
+    {
+        "Brand": "Diptyque",
+        "Neighborhood": "Columbus Circle",
+        "Address": "10 Clumbus Circle",
+        "Full Address": "10 Columbus Cir, New York, NY 10019",
+        "Latitude": 40.7685526,
+        "Longitude": -73.9831866
+    },
+    {
+        "Brand": "Diptyque",
+        "Neighborhood": "Upper East Side",
+        "Address": "971 Madison Ave",
+        "Full Address": "971 Madison Ave, New York, NY 10021",
+        "Latitude": 40.7741808,
+        "Longitude": -73.9633844
+    },{
+        "Brand": "Warby Parker",
+        "Neighborhood": "Soho",
+        "Address": "121 Greene Street",
+        "Full Address": "121 Greene St, New York, NY 10012",
+        "Latitude": 40.7254012,
+        "Longitude": -73.9992147
+    },
+    {
+        "Brand": "Warby Parker",
+        "Neighborhood": "Soho",
+        "Address": "233 Spring Street",
+        "Full Address": "233 Spring St, New York, NY 10013",
+        "Latitude": 40.7257355,
+        "Longitude": -74.0048856
+    },
+    {
+        "Brand": "Warby Parker",
+        "Neighborhood": "Union Square",
+        "Address": "73 Fifth Ave",
+        "Full Address": "73 5th Ave, New York, NY 10003",
+        "Latitude": 40.7367004,
+        "Longitude": -73.9928107
+    },
+    {
+        "Brand": "Warby Parker",
+        "Neighborhood": "Meatpacking",
+        "Address": "819 Washington",
+        "Full Address": "819 Washington St, New York, NY 10014",
+        "Latitude": 40.739644,
+        "Longitude": -74.0078344
+    },
+    {
+        "Brand": "Warby Parker",
+        "Neighborhood": "Grand Central",
+        "Address": "105 East 42nd",
+        "Full Address": "105 E 42nd St, New York, NY 10017",
+        "Latitude": 40.7520368,
+        "Longitude": -73.9765835
+    },
+    {
+        "Brand": "Warby Parker",
+        "Neighborhood": "Rockefeller Center",
+        "Address": "1258 Avenue of the Americas ",
+        "Full Address": "1258 6th Ave, New York, NY 10020",
+        "Latitude": 40.7597073,
+        "Longitude": -73.9802711
+    },
+    {
+        "Brand": "Warby Parker",
+        "Neighborhood": "Lincoln Center",
+        "Address": "185 Columbus Ave",
+        "Full Address": "185 Columbus Ave, New York, NY 10023",
+        "Latitude": 40.7749073,
+        "Longitude": -73.9803721
+    },
+    {
+        "Brand": "Warby Parker",
+        "Neighborhood": "Upper West Side",
+        "Address": "424 Columbus Ave",
+        "Full Address": "424 Columbus Ave, New York, NY 10024",
+        "Latitude": 40.7829821,
+        "Longitude": -73.9751378
+    },
+    {
+        "Brand": "Warby Parker",
+        "Neighborhood": "Morningside Heights",
+        "Address": "2875 Broadway",
+        "Full Address": "2875 Broadway, New York, NY 10025",
+        "Latitude": 40.8053727,
+        "Longitude": -73.9662439
+    },
+    {
+        "Brand": "Warby Parker",
+        "Neighborhood": "Upper East Side",
+        "Address": "1209 Lexinton Ave",
+        "Full Address": "1209 Lexington Ave, New York, NY 10028",
+        "Latitude": 40.7767461,
+        "Longitude": -73.9573587
+    },
+    {
+        "Brand": "Warby Parker",
+        "Neighborhood": "Williamsburg",
+        "Address": "124 North 6th St",
+        "Full Address": "124 N 6th St, Brooklyn, NY 11249",
+        "Latitude": 40.7179009,
+        "Longitude": -73.9596223
+    },
+    {
+        "Brand": "Warby Parker",
+        "Neighborhood": "Downtown Brooklyn",
+        "Address": "55 Bergen Street",
+        "Full Address": "55 Bergen St, Brooklyn, NY 11201",
+        "Latitude": 40.6871673,
+        "Longitude": -73.991072
+    },
+    {
+        "Brand": "Warby Parker",
+        "Neighborhood": "Prospect Heights",
+        "Address": "208 Flatbush Ave",
+        "Full Address": "208 Flatbush Ave, Brooklyn, NY 11217",
+        "Latitude": 40.6812784,
+        "Longitude": -73.9757141
+    },
+	{
+        "Neighborhood": "Williamsburg",
+        "Full Address": "412 W 13th St, New York, NY 10014",
+        "Address": "193 Bedford",
+        "GF Size": "2,007",
+        "LL Size": "",
+        "Total Size": "2,007",
+        "Asking Rent Per Month": " $ 37,500 ",
+        "Asking Rent Per Year": " $ 450,000 ",
+        "Notes": "",
+        "Image": "https://images.squarespace-cdn.com/content/64c413f541fc0e544918798e/15a5045c-161d-4793-95df-ba29ded39bc3/412+West+13th+Street.png?content-type=image%2Fpng",
+        "Latitude": 40.74064795013613,
+        "Longitude": -74.00671463255092,
+        "Brand": "Properties"
+    },
+    {
+        "Neighborhood": "Upper West Side",
+        "Full Address": "201 Columbus Ave, New York, NY 10023",
+        "Address": "205 Columbus",
+        "GF Size": 900,
+        "LL Size": 335,
+        "Total Size": 1235,
+        "Asking Rent Per Month": " $ 33,750 ",
+        "Asking Rent Per Year": " $ 405,000 ",
+        "Notes": "",
+        "Image": "https://images.squarespace-cdn.com/content/64c413f541fc0e544918798e/e017a904-1b5b-40d3-a108-873eaedf5df7/205+Columbus+Avenue.png",
+        "Latitude": 40.775529,
+        "Longitude": -73.9799668,
+        "Brand": "Properties"
+    },
+    {
+        "Neighborhood": "Meatpacking",
+        "Full Address": "875 Washington St, New York, NY 10014",
+        "Address": "875 Washington ",
+        "GF Size": "1,000",
+        "LL Size": "",
+        "Total Size": "1,000",
+        "Asking Rent Per Month": " $ 29,166 ",
+        "Asking Rent Per Year": " $ 349,992 ",
+        "Notes": "",
+        "Image": "https://images.squarespace-cdn.com/content/64c413f541fc0e544918798e/d74a3648-00ec-454f-a7b8-ff47d2421392/875+Washington+Street.png",
+        "Latitude": 40.7410202,
+        "Longitude": -74.0072325,
+        "Brand": "Properties"
+	}
+];
+
+//This array values are from the brand names. Ex: "Brand": "Properties"
+var allCategories = [
+	"Lora Piana",
+	"Brunello Cucinelli",
+	"Zegna",
+	"Diptyque",
+	"Warby Parker",
+	"Properties"			//This cannot be changed
+	
+
+];
+
+
+var allIcons = [
+	"https://images.squarespace-cdn.com/content/64c413f541fc0e544918798e/38671277-8a7d-4f73-ac4c-c957915e15cb/Green+Marker.png",
+	"https://images.squarespace-cdn.com/content/64c413f541fc0e544918798e/7cb4b5d7-aeec-4bff-9f16-4b83086e79dd/Red+Marker+.png",
+	"https://images.squarespace-cdn.com/content/64c413f541fc0e544918798e/7cb4b5d7-aeec-4bff-9f16-4b83086e79dd/Red+Marker+.png",
+	"https://images.squarespace-cdn.com/content/64c413f541fc0e544918798e/bf97084d-56aa-49f8-9161-39ac39cccf82/Purple+Marker.png",
+	"https://images.squarespace-cdn.com/content/64c413f541fc0e544918798e/a4cf64ce-986a-477a-9cd4-47dfff0e85a4/Orange+Marker.png",
+	"https://images.squarespace-cdn.com/content/64c413f541fc0e544918798e/fd04f87d-68fe-483c-a2ad-4095788b9227/Blue+marker.png"
+
+];
+
+var allCategoryLabels = [
+	"Lora Piana",
+	"Brunello Cucinelli",
+	"Zegna prop",
+	"Diptyque",
+	"Warby Parker",
+	"Opportunities"					//This cannot be changed
+	
+
+
+];
